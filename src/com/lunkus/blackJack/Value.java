9dda09fd558b0b1f9add5a53bcf355dd352a3d84
eincoder <eincoder@gmail.com>
2019-06-26 18:24:43 +0300

    Cards values enums constant class

---
+++ src/com/lunkus/blackJack/Value.java
@@ -0,0 +1,9 @@
+package com.lunkus.blackJack;
+
+/**
+ * Enum class used for value constants in program
+ */
+public enum Value {
+
+    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, KING, QUEEN, ACE;
+}
