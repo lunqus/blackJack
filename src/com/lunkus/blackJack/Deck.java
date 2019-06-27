0109dd45188e647e30518d4fce3a564924ca41d0
eincoder <eincoder@gmail.com>
2019-06-27 08:51:34 +0300

    Deck class populates & shuffles deck of cards

--- src/com/lunkus/blackJack/Deck.java
+++ src/com/lunkus/blackJack/Deck.java
@@ -45,7 +45,56 @@ public class Deck {
         comingFrom.removeCard(0);
     }
 
+    public void moveAllToDeck(Deck moveTo) {
+        int thisDeckSize = cards.size();
+
+        for (int i = 0; i < thisDeckSize; i++) {
+            moveTo.addCard(this.getCard(i));
+        }
+
+        for (int i = 0; i < thisDeckSize; i++) {
+            this.removeCard(0);
+        }
+    }
+
+    // Returns total value of cards in the deck
+    public int cardsValue() {
+        int totalValue = 0;
+        int aces = 0;
+
+        for (Card card : cards) {
+            switch (card.getValue()) {
+                case TWO: totalValue += 2; break;
+                case THREE: totalValue += 3; break;
+                case FOUR: totalValue += 4; break;
+                case FIVE: totalValue += 5; break;
+                case SIX: totalValue += 6; break;
+                case SEVEN: totalValue += 7; break;
+                case EIGHT: totalValue += 8; break;
+                case NINE: totalValue += 9; break;
+                case TEN: totalValue += 10; break;
+                case JACK: totalValue += 10; break;
+                case QUEEN: totalValue += 10; break;
+                case KING: totalValue += 10; break;
+                case ACE: totalValue += 1; break;
+            }
+        }
+
+        for (int i = 0; i < aces; i++) {
+            if (totalValue > 10) {
+                totalValue += 1;
+            } else {
+                totalValue += 11;
+            }
+        }
+
+        return  totalValue;
+    }
+
     // Getters and Setters
+    public int deckSize() {
+        return cards.size();
+    }
     public void removeCard(int i) {
         cards.remove(i);
     }
